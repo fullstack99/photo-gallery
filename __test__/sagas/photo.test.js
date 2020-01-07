f3998b735af4f2d804874cb66e4f64639194a3a5
fullstack99 <success1989127@gmail.com>
2020-01-06 22:12:41 -0300

    photo testing

--- __test__/sagas/photo.test.js
+++ __test__/sagas/photo.test.js
@@ -1,20 +1,26 @@
-import { put, takeEvery } from 'redux-saga/effects';
+import { put, takeEvery, call } from 'redux-saga/effects';
+import { cloneableGenerator } from '@redux-saga/testing-utils';
 import { Creators as Actions, Types } from '../../src/actions';
 import photoSagas from '../../src/sagas/photo';
 import getPhoto from '../../src/sagas/photo';
+import * as API from '../../src/services/api';
+
+const getPhotoAction = Actions.getPhotoRequest();
 
 describe('photoSagas', () => {
-  // it('should dispatch action "GET_PHOTO_REQUEST" ', () => {
-  //   const generator = photoSagas();
-  //   expect(generator.next().value).toEqual(takeEvery(Types.GET_PHOTO_REQUEST, getPhoto));
-  //   expect(generator.next().done).toBeTruthy();
-  // })
+  const generator = cloneableGenerator(getPhoto)();
+  expect(generator.next().value).toEqual(takeEvery(Types.GET_PHOTO_REQUEST, getPhoto));
+  expect(generator.next().value).toEqual(call(API.getPhotos));
 
-  // it('should dispatch action "GET_PHOTO_SUCCESS" with result from fetch News API', () => {
-  //   const mockResponse = {"status": 200};
-  //   const generator = getPhoto();
-  //   generator.next();
-  //   expect(generator.next(mockResponse).value).toEqual(put({type:"GET_PHOTO_SUCCESS"}))
-  //   expect(generator.next().done).toBeTruthy();
-  // })
+  test('get Photo success', () => {
+    const clone = generator.clone();
+    expect(clone.next(true).value).toEqual(put(Actions.getPhotoSuccess()));
+    expect(clone.next().done).toEqual(true);
+  });
+
+  test('get Photo Failure', () => {
+    const clone = generator.clone();
+    expect(clone.next(false).value).toEqual(put(Actions.getPhotoFailure()));
+    expect(clone.next().done).toEqual(true);
+  });
 })
